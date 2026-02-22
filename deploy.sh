85efc4d60f178243aa583eb5b04c5bf409ed0c22
adilarshidin <adil.therunningman@gmail.com>
2026-02-22 20:32:34 +0100

    fix: load nvm in shell

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@ echo "Deploy script"
 
 # add the commands here
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.4/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
 nvm install 16
 nvm use 16
 node app.js
